c1daefeaed8eed1080ff05d5edcc80a22133200f
fjh <1240420268@qq.com>
2022-10-13 20:52:00 +0800

    dev修改了helloworld

--- src/main/java/com/example/gitstudytestdemo/HellolWorld.java
+++ src/main/java/com/example/gitstudytestdemo/HellolWorld.java
@@ -2,6 +2,6 @@ package com.example.gitstudytestdemo;
 
 public class HellolWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("HelloWorld!--修改dev分支");
     }
 }
