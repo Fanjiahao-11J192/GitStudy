d8fd2864d2d67293d76a21d7586199a5d73a67bc
fjh <1240420268@qq.com>
2022-10-13 18:30:20 +0800

    第一次全部提交

---
+++ src/main/java/com/example/gitstudytestdemo/GitStudyTestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitstudytestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitStudyTestDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitStudyTestDemoApplication.class, args);
+    }
+
+}
